c0acd00839be2793ca286f5ecef23a07e7910cdd
Tegnio <olegbubble@gmail.com>
2021-05-20 09:30:28 +0300

    new 'avatar' command

---
+++ commands/useful/avatar.js
@@ -0,0 +1,19 @@
+module.exports = {
+  name: "avatar",
+  category: "useful",
+  aliases: ["ava", "av"],
+  cooldown: 2,
+  botPermissions: ["ATTACH_FILES", "EMBED_LINKS"],
+  async execute(bot, message, args) {
+    const lang = await bot.getGuildLang(message.guild.id);
+    const member = bot.findMember(message, args, true);
+    const avatar = member.user.displayAvatarURL({ format: "png", size: 2048, dynamic: true });
+
+    const embed = bot.buildEmbed(message)
+      .setTitle(lang.MEMBER.AVATAR.replace("{username}", member.user.username))
+      .setURL(avatar)
+      .setImage(avatar);
+
+    message.channel.send(embed);
+  },
+};
