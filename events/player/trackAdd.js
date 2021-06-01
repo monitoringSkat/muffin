e6f35a62ff06188cd2907c54709967988cc57d3c
Tegnio <olegbubble@gmail.com>
2021-06-01 13:56:18 +0300

    added track queue position

--- events/player/trackAdd.js
+++ events/player/trackAdd.js
@@ -1,18 +1,17 @@
 module.exports = {
     name: "trackAdd",
     type: "player",
-    async execute(bot, message, queue, track, i) {
+    async execute(bot, message, queue, track) {
       const lang = await bot.getGuildLang(message.guild.id);
 
       const embed = bot.buildEmbed(message)
-      .setAuthor(lang.MUSIC.QUEUED)
+      .setAuthor(lang.MUSIC.QUEUED.replace("{pos}", queue.tracks.length - 1))
       .setTitle(track.title)
       .setURL(track.url)
       .setThumbnail(track.thumbnail)
       .addField(lang.OTHER.REQUESTED_BY, track.requestedBy.tag, true)
       .addField(lang.MUSIC.UPLOADED_BY, track.author, true)
       .addField(lang.MUSIC.DURATION, track.duration, true);
-
 
       return message.reply(embed);
     },
