5f5b93315f846dddde300ff8da14fe3046beb0ee
Tegnio <olegbubble@gmail.com>
2021-06-04 03:26:54 +0300

    'blacklisted' option is now required

--- models/User.model.js
+++ models/User.model.js
@@ -3,7 +3,7 @@ const { model, Schema } = require("mongoose");
 const userSchema = new Schema({
   user_id: { type: String, required: true },
   guild_id: { type: String, required: true },
-  blacklisted: { type: Boolean, default: false },
+  blacklisted: { type: Boolean, default: false, required: true },
 });
 
 module.exports = model("User", userSchema);
