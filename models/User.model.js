bee98bf9fe0808c0a1810f3dccc8f86fea8aac08
Tegnio <olegbubble@gmail.com>
2021-06-05 16:56:23 +0300

    remove 'blacklisted' property of user

--- models/User.model.js
+++ models/User.model.js
@@ -3,7 +3,6 @@ const { model, Schema } = require("mongoose");
 const userSchema = new Schema({
   user_id: { type: String, required: true },
   guild_id: { type: String, required: true },
-  blacklisted: { type: Boolean, default: false, required: true },
 });
 
 module.exports = model("User", userSchema);
