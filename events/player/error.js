fcd3f2178d4189c6849ac7cb94c52e3bbb3efef6
Tegnio <olegbubble@gmail.com>
2021-06-06 22:09:29 +0300

    add replies for player events

--- events/player/error.js
+++ events/player/error.js
@@ -6,28 +6,28 @@ module.exports = {
   
       switch (error) {
         case "UnableToJoin": {
-          return message.channel.send(lang.MUSIC.JOIN_ERROR);
+          return message.reply(lang.MUSIC.JOIN_ERROR);
         }
         case "NotConnected": {
-          return message.channel.send(lang.MUSIC.MUST_BE_IN_VC);
+          return message.reply(lang.MUSIC.MUST_BE_IN_VC);
         }
         case "NotPlaying": {
-          return message.channel.send(lang.MUSIC.EMPTY_QUEUE);
+          return message.reply(lang.MUSIC.EMPTY_QUEUE);
         }
         case "ParseError": {
-          return message.channel.send(lang.MUSIC.FETCH_ERROR);
+          return message.reply(lang.MUSIC.FETCH_ERROR);
           bot.player.skip(message);
         }
         case "VideoUnavailable": {
-          return message.channel.send(lang.MUSIC.TRACK_UNAVAILABLE);
+          return message.reply(lang.MUSIC.TRACK_UNAVAILABLE);
           bot.player.skip(message);
         }
         case "MusicStarting": {
-          return message.channel.send(lang.MUSIC.TRACK_STILL_LOADING);
+          return message.reply(lang.MUSIC.TRACK_STILL_LOADING);
         }
         default: {
           bot.sendErrorLog(bot, { stack: error, name: "discord-player" }, "error");
-          return message.channel.send(`${lang.GLOBAL.ERROR}\n\n\`\`\`${error?.stack || e}\`\`\``);
+          return message.reply(`${lang.GLOBAL.ERROR}\n\n\`\`\`${error?.stack || e}\`\`\``);
         }
       }
     },
