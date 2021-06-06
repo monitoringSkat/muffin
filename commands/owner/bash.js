6c8bca008ba8056d5bb6d989792965fc02eb9863
Tegnio <olegbubble@gmail.com>
2021-06-06 22:05:51 +0300

    add replies for 'owner' category

--- commands/owner/bash.js
+++ commands/owner/bash.js
@@ -7,13 +7,13 @@ module.exports = {
     const lang = await bot.getGuildLang(message.guild.id);
 
     if (!args) {
-      return message.channel.send(bot.lang.GLOBAL.PROVIDE_ARGS);
+      return message.reply(bot.lang.GLOBAL.PROVIDE_ARGS);
     }
 
     const result = await require('child_process')
     .execSync(args.join(' '))
     .toString('utf8');
 
-    message.channel.send(result, { split: true });
+    message.reply(result, { split: true });
   }
 };
