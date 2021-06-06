f2c16ea7f897aeb5cf1728384403834f247b7eb3
Tegnio <olegbubble@gmail.com>
2021-06-06 16:46:47 +0300

    rewrite RateLimit event

--- events/bot/rateLimit.js
+++ events/bot/rateLimit.js
@@ -3,7 +3,7 @@ module.exports = {
   type: "bot",
   execute(bot, rateLimitInfo) {
     if(bot.config.debug) {
-      console.log(`[Warning] ${rateLimitInfo}`)
+      console.log(`[Rate Limit] Rate-limited for ${rateLimitInfo.timeout}ms on ${rateLimitInfo.route} route for hitting ${rateLimitInfo.limit} messages limit`)
     }
   },
 };
